abf84144742b0766e1f2628905819b350d5ebd4e
Hyunjun KIM <hihj070914@icloud.com>
2023-11-29 16:31:40 +0900

    feat: local db connection

--- assignment1/src/app.module.ts
+++ assignment1/src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true, // 전체적으로 사용하기 위해
+            envFilePath: `.env`,
+        }),
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: 'localhost',
+            port: +process.env.DB_PORT,
+            username: process.env.MYSQL_USER_ID,
+            password: process.env.MYSQL_PASSWORD,
+            database: process.env.DB_NAME,
+            entities: [],
+            synchronize: true,
+        }),
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}

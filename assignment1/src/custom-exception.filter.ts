6c47e5e74c3dbb64028c1408932e004cad0f7022
Hyunjun KIM <hihj070914@icloud.com>
2023-11-30 19:18:36 +0900

    feat: login redirection - 401 에러에 대해 로그인 페이지로 리다이렉션 하는 필터를 추가하였습니다.

--- assignment1/src/custom-exception.filter.ts
+++ assignment1/src/custom-exception.filter.ts
@@ -13,6 +13,9 @@ export class CustomExceptionsFilter implements ExceptionFilter {
         if (exception instanceof Error && exception.message.includes('ENOENT')) {
             this.logger.error(`redirect to 404 page. reason - ${exception.message}`);
             response.status(404).sendFile(join(__dirname, '..', 'public', '404.html'));
+        } else if (exception instanceof Error && status === 401) {
+            this.logger.error(`redirect to lgoin page. reason - ${exception.message}`);
+            response.status(401).sendFile(join(__dirname, '..', 'public', 'login.html'));
         } else {
             this.logger.error(exception instanceof Error ? exception.stack : exception);
             response.status(status).json({

7cf0715288c25aae513b2e3b1420a46c7e952d4a
Hyunjun KIM <hihj070914@icloud.com>
2023-11-30 14:34:35 +0900

    feat: check duplicated id - 중복된 id를 체크하기 위한 엔드포인트를 구성하였습니다.

--- assignment1/src/auth/auth.controller.ts
+++ assignment1/src/auth/auth.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Logger, Post } from '@nestjs/common';
+import { Body, Controller, Logger, Post, Req, Res } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import { AuthService } from './auth.service';
+import { LoginDto } from './login.dto';
+import { RegisterDto } from './register.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -11,14 +13,35 @@ export class AuthController {
     ) {}
 
     @Post('login')
-    login(): string {
+    async login(@Body() loginDto: LoginDto, @Res() res): Promise<void> {
         this.logger.log('endpoint /auth/login called');
-        return this.authService.login();
+        const isSuccess: boolean = await this.authService.login(loginDto);
+        if (isSuccess) {
+            res.redirect('/');
+        } else {
+            res.status(401).send('Login failed');
+        }
     }
 
     @Post('register')
-    register(): string {
+    async register(@Body() registerDto: RegisterDto, @Res() res) {
         this.logger.log('endpoint /auth/register called');
-        return this.userService.register();
+        try {
+            await this.userService.registerUser(registerDto);
+            res.status(200).redirect('/');
+        } catch (e) {
+            res.status(409).send('User already exists');
+        }
+    }
+
+    @Post('checkDuplication')
+    async checkDuplication(@Body('id') id: string, @Res() res): Promise<void> {
+        this.logger.log('endpoint /auth/checkDuplication called');
+        try {
+            await this.userService.findUserById(id);
+            res.status(409).send('User already exists');
+        } catch (e) {
+            res.status(200).send('User does not exist');
+        }
     }
 }

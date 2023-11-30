e5f739aba3bedf000764aafbddbc64e3b1c04d4a
Hyunjun KIM <hihj070914@icloud.com>
2023-11-30 14:35:46 +0900

    feat: register service - 회원가입 비즈니스 로직을 추가하였습니다.

--- assignment1/src/auth/auth.service.ts
+++ assignment1/src/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
+import { LoginDto } from './login.dto';
 
 @Injectable()
 export class AuthService {
@@ -9,4 +11,18 @@ export class AuthService {
         private jwtService: JwtService,
         private userService: UserService,
     ) {}
+
+    async login(loginDto: LoginDto): Promise<boolean> {
+        this.logger.log('login() called');
+        console.log(loginDto);
+        return false;
+    }
+
+    async validateUser(id: string, password: string): Promise<any> {
+        this.logger.log('validateUser() called');
+        const user = await this.userService.findUserById(id);
+        if (bcrypt.compare(password, user.password)) {
+        }
+        return null;
+    }
 }
--- assignment1/src/user/user.service.ts
+++ assignment1/src/user/user.service.ts
@@ -1,4 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { UserRepository } from './user.repository';
+import { RegisterDto } from 'src/auth/register.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    private logger = new Logger(UserService.name);
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepository: UserRepository,
+    ) {}
+
+    async findUserById(id: string): Promise<User> {
+        const user: User = await this.userRepository.findOne({ where: { userId: id } });
+        if (user) {
+            return user;
+        } else throw new NotFoundException();
+    }
+
+    async registerUser(registerDto: RegisterDto): Promise<User> {
+        try {
+            const { id, password } = registerDto;
+            const hashedPassword = await bcrypt.hash(password, 12);
+            const newUser = this.userRepository.create({
+                userId: id,
+                password: hashedPassword,
+            } as User);
+            const user = await this.userRepository.save(newUser);
+            return user;
+        } catch (error) {
+            if (error.code === '23505') throw new ConflictException('Existing userName');
+            else throw new InternalServerErrorException('from registerUser');
+        }
+    }
+}

a5c11917372b237283650fcb36314c32450c018d
Hyunjun KIM <hihj070914@icloud.com>
2023-12-05 18:57:16 +0900

    fix: socket module - 누락되었던 UserService 의존성을 추가하였습니다.

--- assignment2/src/socket/socket.module.ts
+++ assignment2/src/socket/socket.module.ts
@@ -5,10 +5,11 @@ import { UserModule } from 'src/user/user.module';
 import { SocketGateway } from './socket.gateway';
 import { SocketService } from './socket.service';
 import { JwtService } from '@nestjs/jwt';
+import { UserService } from 'src/user/user.service';
 
 @Module({
     imports: [UserModule, TypeOrmModule.forFeature([User])],
-    providers: [SocketGateway, SocketService, JwtService],
+    providers: [SocketGateway, SocketService, JwtService, UserService],
     exports: [SocketGateway],
 })
 export class SocketModule {}
